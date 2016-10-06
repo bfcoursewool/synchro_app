4483cf3c7cf4e257d84948df96efcaaf2aab192d
Rob (local) <rob.m.kotz@gmail.com>
2016-10-05 17:02:39 -0700

    set up static assets correctly so we dont need extra routes

--- synchro_app.py
+++ synchro_app.py
@@ -11,13 +11,5 @@ def hello_world():
 def bootstrap_test():
   return render_template('carousel_test.html')
 
-@app.route('/bootstrap-3.3.7/<path:path>')
-def bootstrap_dir(path):
-  return send_from_directory('bootstrap-3.3.7', path)
-
-@app.route('/css/<path:path>')
-def css_dir(path):
-  return send_from_directory('css', path)
-
 if __name__ == '__main__':
   app.run(debug=True)
