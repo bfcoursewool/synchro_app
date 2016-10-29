de1b2c64749eade089125a35b329aa8683abcf18
Rob (local) <rob.m.kotz@gmail.com>
2016-10-28 23:19:59 -0700

    need a leading slash on the admin blueprint url prefix

--- synchro/admin/controllers.py
+++ synchro/admin/controllers.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-admin_section = Blueprint('admin_section', __name__, url_prefix='admin')
+admin_section = Blueprint('admin_section', __name__, url_prefix='/admin')
 
 @admin_section.route('/')
 def admin():
