f39be438644e5dbca6706187d3b21b88bd8986ca
Rob (local) <rob.m.kotz@gmail.com>
2016-12-24 16:44:00 +0530

    refactor controller to work with our new domain and subdomain setup

--- synchro/landing_pages/controllers.py
+++ synchro/landing_pages/controllers.py
@@ -1,14 +1,16 @@
+from urlparse import urlparse
 from flask import (
   Blueprint, 
   render_template, 
   make_response,
-  abort
+  abort,
+  request
 )
 from synchro.third_party_resources import bootstrap, bootstrap_css
 from synchro.resources import synchro_shopify, synchro_buy_button, synchro_effects, gold_analytics
 from synchro.third_party_resources import wowjs
 
-landing_pages = Blueprint('landing_pages', __name__, url_prefix='/pages')
+landing_pages = Blueprint('landing_pages', __name__)
 
 endpoint_info_dict = {
   'genesis': {
@@ -25,14 +27,6 @@ endpoint_info_dict = {
   },
   'gold': {
     'v0': {
-      'template': 'gold/gold.html',
-      'scripts': [synchro_shopify]
-    }, 
-    'v1': {
-      'template': 'gold/gold_v1.html', 
-      'scripts': [synchro_buy_button]
-    }, 
-    'af': {
       'template': 'gold/gold_af.html',
       'scripts': [synchro_buy_button, synchro_effects, gold_analytics, wowjs]
     }
@@ -49,13 +43,22 @@ endpoint_info_dict = {
   }
 }
 
+@landing_pages.route('/', defaults={'page': 'none', 'version': 'v0'})
 @landing_pages.route('/<page>/', defaults={'version': 'v0'})
 @landing_pages.route('/<page>/<version>')
-def landing_page(page, version):
+def landing_page(page, version): 
+  parsed_url = urlparse(request.url_root)
+  host = parsed_url[1].split(':')[0] # Don't care about port, if it's in the netloc
+  subdomain = host.split('.')[0]
+
+  if subdomain != "dev" and host != "localhost": 
+    page = subdomain
+
   # TODO -- would prefer some form of assertion here so we can get visibility into people
   # requesting weird URLs but for now throwing a 404 will work. 
   if page not in endpoint_info_dict or version not in endpoint_info_dict[page]: 
     abort(404)
+
   # Assert that each version entry in the info_dict contains both a template to render and a list of scripts to include. 
   # A failed assertion should happen only during development, so this helps ensure developer consistency. 
   assert 'template' in endpoint_info_dict[page][version]
