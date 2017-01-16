555429572113eabeb1a83903affdb3e3e917895c
Rob (local) <rob.m.kotz@gmail.com>
2017-01-16 19:13:20 +0530

    set up funnel steps and mark the first one complete on click of checkout button

--- synchro/frontend_build/compiled/buy_button.js
+++ synchro/frontend_build/compiled/buy_button.js
@@ -111,6 +111,9 @@ $(document).ready(function() {
         'openCheckout': function(cart) {
           _kmq.push(['alias', cart.model.id, kissIdentity]); 
           _kmq.push(['record', 'started purchase', {}]);
+          ga('ec:setAction','checkout', {
+            'step': 1,
+          });
           window.location.href = cart.model.checkoutUrl;
         }
       }
