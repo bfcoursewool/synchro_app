6158eedc20e0920776adf3e5e5f25a3e9bd98588
Rob (local) <rob.m.kotz@gmail.com>
2017-01-20 15:43:35 +0530

    again...

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
-  'flask',
+  'Flask',
   'Flask-MySQLdb',
   'SQLAlchemy',
   'fanstatic',
