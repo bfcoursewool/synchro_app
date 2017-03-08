fbcc375dec7fa401818c4465968568d6b0e01505
Rob (local) <rob.m.kotz@gmail.com>
2017-03-08 15:13:40 +0530

    really fix the routing issue and add is_variant concept for nofollow tags

--- synchro/views/landing_pages/controllers.py
+++ synchro/views/landing_pages/controllers.py
@@ -31,6 +31,7 @@ endpoint_info_dict = {
       'template': 'landing_pages/gold/gold_organic.html',
       'scripts': [synchro_buy_button, synchro_effects, gold_analytics, wowjs, videojsie8, videojsga],
       'template_vars': {
+        'is_variant': False,
         'poster_image': 'http://cdn.besynchro.com/gold/gold-video-poster2.jpg',
         'stylesheet': 'gold/gold_video.css'
       }
@@ -39,6 +40,7 @@ endpoint_info_dict = {
       'template': 'landing_pages/gold/gold_paid.html',
       'scripts': [synchro_buy_button, synchro_effects, gold_analytics, wowjs, videojsie8, videojsga],
       'template_vars': {
+        'is_variant': True,
         'poster_image': 'http://cdn.besynchro.com/gold/gold-video-poster2.jpg',
         'stylesheet': 'gold/gold_video.css'
       }
@@ -69,8 +71,10 @@ def landing_page(page, version):
   # let's not set the page to 3 numerical digits.
   if const.kENVIRONMENT == 'production' and subdomain.isalpha():
     # For addresses like: gold.besynchro.com/1 the first arg is actually the version, but it comes 
-    # through here as "page" because of how the route is set up... 
-    version = page
+    # through here as "page" because of how the route is set up, so we'll set that as the version
+    # so long as it's not "none", in which case version is already v0
+    if page is not 'none':
+      version = page
     page = subdomain
 
   # Page and version can also be passed in as GET vars, for URL-formatting reasons
