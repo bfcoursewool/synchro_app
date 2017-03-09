4e92dd8a9ff4c0c80da58139989f7090e4bd0314
Syed AtiF Husain <atif@afstudios.me>
2017-03-09 19:00:36 +0800

    [ABTEST] Edited  controller nesting

--- synchro/views/landing_pages/controllers.py
+++ synchro/views/landing_pages/controllers.py
@@ -56,25 +56,23 @@ endpoint_info_dict = {
       'scripts': [synchro_shopify]
     }
   },
-  'abtests': {
-    'cro002': {
-      'v1' : {
-        'template': 'landing_pages/gold/gold_cro002-1.html',
-        'scripts': [synchro_buy_button, synchro_effects, gold_analytics, wowjs, videojsie8, videojsga],
-        'template_vars': {
-          'is_variant': False,
-          'poster_image': 'http://cdn.besynchro.com/gold/gold-video-poster2.jpg',
-          'stylesheet': 'gold/gold_cro002.css'
-        }
-      },
-      'v2' : {
-        'template': 'landing_pages/gold/gold_cro002-2.html',
-        'scripts': [synchro_buy_button, synchro_effects, gold_analytics, wowjs, videojsie8, videojsga],
-        'template_vars': {
-          'is_variant': False,
-          'poster_image': 'http://cdn.besynchro.com/gold/gold-video-poster2.jpg',
-          'stylesheet': 'gold/gold_cro002.css'
-        }
+  'cro002': {
+    'v1' : {
+      'template': 'landing_pages/gold/gold_cro002-1.html',
+      'scripts': [synchro_buy_button, synchro_effects, gold_analytics, wowjs, videojsie8, videojsga],
+      'template_vars': {
+        'is_variant': False,
+        'poster_image': 'http://cdn.besynchro.com/gold/gold-video-poster2.jpg',
+        'stylesheet': 'gold/gold_cro002.css'
+      }
+    },
+    'v2' : {
+      'template': 'landing_pages/gold/gold_cro002-2.html',
+      'scripts': [synchro_buy_button, synchro_effects, gold_analytics, wowjs, videojsie8, videojsga],
+      'template_vars': {
+        'is_variant': False,
+        'poster_image': 'http://cdn.besynchro.com/gold/gold-video-poster2.jpg',
+        'stylesheet': 'gold/gold_cro002.css'
       }
     }
   }
