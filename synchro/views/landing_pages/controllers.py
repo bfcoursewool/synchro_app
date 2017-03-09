b013b6a82d7c3ad9b05f0a9ae0b1e2895831b0ba
Syed AtiF Husain <atif@afstudios.me>
2017-03-09 18:24:18 +0800

    [ABTEST] New branch and updated controller

--- synchro/views/landing_pages/controllers.py
+++ synchro/views/landing_pages/controllers.py
@@ -55,6 +55,26 @@ endpoint_info_dict = {
       'template': 'landing_pages/digestcleanse/digestcleanse_v1.html',
       'scripts': [synchro_shopify]
     }
+  },
+  'abtests': {
+    'cro002-1' : {
+      'template': 'landing_pages/gold/gold_cro002-1.html',
+      'scripts': [synchro_buy_button, synchro_effects, gold_analytics, wowjs, videojsie8, videojsga],
+      'template_vars': {
+        'is_variant': False,
+        'poster_image': 'http://cdn.besynchro.com/gold/gold-video-poster2.jpg',
+        'stylesheet': 'gold/gold_cro002.css'
+      }
+    },
+    'cro002-2' : {
+      'template': 'landing_pages/gold/gold_cro002-2.html',
+      'scripts': [synchro_buy_button, synchro_effects, gold_analytics, wowjs, videojsie8, videojsga],
+      'template_vars': {
+        'is_variant': False,
+        'poster_image': 'http://cdn.besynchro.com/gold/gold-video-poster2.jpg',
+        'stylesheet': 'gold/gold_cro002.css'
+      }
+    }
   }
 }
 
@@ -70,7 +90,7 @@ def landing_page(page, version):
   # in other words if this is a health-check request going directly to an instance's ephemeral ip
   # let's not set the page to 3 numerical digits.
   if const.kENVIRONMENT == 'production' and subdomain.isalpha():
-    # For addresses like: gold.besynchro.com/1 the first arg is actually the version, but it comes 
+    # For addresses like: gold.besynchro.com/1 the first arg is actually the version, but it comes
     # through here as "page" because of how the route is set up, so we'll set that as the version
     # so long as it's not "none", in which case version is already v0
     if page is not 'none':
