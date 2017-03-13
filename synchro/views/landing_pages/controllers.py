677d41dda076560d42338acd4f219768cf54b77d
Syed AtiF Husain <atif@afstudios.me>
2017-03-14 01:04:52 +0800

    [ENHANCEMENT] Implemented winner of CRO-002

--- synchro/views/landing_pages/controllers.py
+++ synchro/views/landing_pages/controllers.py
@@ -42,7 +42,7 @@ endpoint_info_dict = {
       'template_vars': {
         'is_variant': True,
         'poster_image': 'http://cdn.besynchro.com/gold/gold-video-poster2.jpg',
-        'stylesheet': 'gold/gold_video.css'
+        'stylesheet': 'gold/gold_testimonial.css'
       }
     }
   },
