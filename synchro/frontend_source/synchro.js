b9c74437a3ca93a403e94608e57889a5c240e966
jeffpowrs <hello@jeffpow.rs>
2017-05-15 17:32:02 -0700

    Benefits Section
    Updates to the benefits section

--- synchro/frontend_source/synchro.js
+++ synchro/frontend_source/synchro.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import LPEffects from './ES6/effects';
+import Togglify from './ES6/toggle';
 import BuyButton from './ES6/ShopifyBuyButton/buy_button';
 import FaceBookPixel from './ES6/analytics/facebook_pixel';
 import Kissmetrics from './ES6/analytics/kissmetrics'; 
@@ -8,6 +9,7 @@ import GradientEffect from './ES6/gradient_effect';
 import SVGLoader from './ES6/svg_loader';
 
 const lpEffectsObj = new LPEffects();
+const togglify = new Togglify();
 const KMObj = new Kissmetrics(); 
 const fbPixelObj = new FaceBookPixel(); 
 const buyButtonObj = new BuyButton(KMObj.KMPromise);
