51c28a522821542be0a79c5ff63e544265080dea
jeffpowrs <hello@jeffpow.rs>
2017-06-20 15:29:15 -0700

    Animating the toggle icon.

--- synchro/frontend_source/ES6/toggle.js
+++ synchro/frontend_source/ES6/toggle.js
@@ -28,10 +28,12 @@ export default class Togglify extends EventsBase {
     if (this.isToggleOpen(targetId)) {
       this.closeToggle(targetId)
       this.setToggleId(targetId, false);
+      this.rotateToggleOff(targetId);
     }
     else {
       this.openToggle(targetId);
       this.setToggleId(targetId, true);
+      this.rotateToggleOn(targetId);
     }
 
   }
@@ -46,12 +48,29 @@ export default class Togglify extends EventsBase {
     $(selector).slideUp();
   }
 
+  rotateToggleOn(toggleId) {
+    const selector = this.getToggleSelector(toggleId);
+    $(selector).addClass('opened');
+    console.log('rotateToggleOn');
+  }
+
+  rotateToggleOff(toggleId) {
+    const selector = this.getToggleSelector(toggleId);
+    $(selector).removeClass('opened');
+    console.log('rotateToggleOff');
+  }
+
+
   setToggleId(toggleId, value) {
     this.toggleIds[toggleId] = value
   }
 
   getContentSelector(toggleId) {
     return `[data-toggle-content='${toggleId}']`;
+  }
+
+  getToggleSelector(toggleId) {
+    return `[data-toggle-id='${toggleId}']`;
   }
 
 }
