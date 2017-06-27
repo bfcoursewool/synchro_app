a242f581da472cce8564300c6faa970f3bfa8924
Rob (local) <rob.m.kotz@gmail.com>
2017-06-27 22:56:07 +0530

    fix fb pixel AddToCart fire

--- synchro/frontend_source/ES6/analytics/facebook_pixel.js
+++ synchro/frontend_source/ES6/analytics/facebook_pixel.js
@@ -18,11 +18,14 @@ export default class FaceBookPixel extends EventsBase {
     price = price.replace(/\$/g, ''); 
     price = price.match(/(.*)\.[0-9][0-9]/); 
     let contentName = $(target).closest('.shopify-buy-frame').find('.shopify-buy__product__title').html() + $(target).closest('.shopify-buy-frame').find('.shopify-buy__product__variant-title').html();
+    let content_ids = [$(target).closest('.purchase__item').attr('data-product-id')];
     let property = $(target).closest('.purchase').attr('data-fb-property');
 
     fbq('track', 'AddToCart', {
       property: property,
       content_name: contentName,
+      content_ids: content_ids,
+      content_type: 'product_group',
       value: price,
       currency: 'USD'
     });
