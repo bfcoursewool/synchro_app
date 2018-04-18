d5bb6922d08a43fd8e075e1b5980ba1c2f8f2461
Kotz <rob.m.kotz@gmail.com>
2018-04-17 14:06:25 -1000

    health-check requests were getting into an infinite redirect, so this fixes that.

--- synchro/views/landing_pages/controllers.py
+++ synchro/views/landing_pages/controllers.py
@@ -192,10 +192,11 @@ def landing_page(page, version):
       version = page
     page = subdomain
 
-  if request.headers.get('X-Forwarded-Proto', '').lower() != "https":
-    full_url = request.url 
-    ssl_url = full_url.replace('http://', 'https://')
-    return redirect(ssl_url)
+    ## Redirect to https if this isn't a health-checker request
+    if request.headers.get('X-Forwarded-Proto', '').lower() != "https":
+      full_url = request.url 
+      ssl_url = full_url.replace('http://', 'https://')
+      return redirect(ssl_url)
 
   # Page and version can also be passed in as GET vars, for URL-formatting reasons
   if 'p' in request.args:
