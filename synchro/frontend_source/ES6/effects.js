1452e7e5993da79d06b7279df3e239dc67b27c4c
AtiF <atif@afstudios.me>
2018-10-09 02:34:11 +0800

    Updated timer on banner

--- synchro/frontend_source/ES6/effects.js
+++ synchro/frontend_source/ES6/effects.js
@@ -123,3 +123,31 @@ export default class LPEffects extends EventsBase {
     });
   }
 }
+
+// Set the date we're counting down to
+var countDownDate = new Date("October 10 2018 23:59:59 UTC-0700").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor(distance / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="timer"
+  document.getElementById("timer").innerHTML = hours + "h " + minutes + "m " + seconds + "s";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
