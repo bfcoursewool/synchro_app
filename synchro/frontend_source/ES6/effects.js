f91bf9d8dcca67a33dd0b16f23691b33c07f547f
Syed AtiF Husain <atif@afstudios.me>
2019-02-26 22:20:18 +0800

    Complete build for cro015

--- synchro/frontend_source/ES6/effects.js
+++ synchro/frontend_source/ES6/effects.js
@@ -168,3 +168,12 @@ export default class LPEffects extends EventsBase {
     });
   }
 }
+$(".video-modal").click(function () {
+  var theModal = $(this).data("target"),
+      videoSRC = $(this).attr("data-video"),
+      videoSRCauto = videoSRC + "?modestbranding=1&rel=0&cc_load_policy=1&showinfo=0&html5=1&autoplay=1";
+  $(theModal + ' iframe').attr('src', videoSRCauto);
+  $(theModal).on('hidden.bs.modal', function () {
+    $(theModal + ' iframe').attr('src', videoSRC);
+  });
+});
