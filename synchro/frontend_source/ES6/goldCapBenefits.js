4290e625e74d5901f22525d7956ed88ce7dccd96
Rob Kotz <rob.m.kotz@gmail.com>
2020-03-25 17:34:01 -1000

    woop woop

--- synchro/frontend_source/ES6/goldCapBenefits.js
+++ synchro/frontend_source/ES6/goldCapBenefits.js
@@ -6,75 +6,38 @@ export default class GoldCapBenefits {
   constructor() {
     this._controller = new ScrollMagic.Controller()
     $(() => {
-      let scene = new ScrollMagic.Scene({ triggerElement: '.benefits-slider__content', duration: 2300})
-                      .setPin('.benefits-slider__content')
+      let scene = new ScrollMagic.Scene({ triggerElement: '.pin-trigger', duration: 2300})
+                      .setPin('.pinned-section')
                       .addTo(this._controller)
-      new ScrollMagic.Scene({ 
-            triggerElement: '.benefits-slider__copy1', 
-            triggerHook: 0.9, // show, when scrolled 10% into view
-            duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-            offset: 50, // move trigger to center of element
-          })
-          .setClassToggle('.benefits-slider__copy1', 'active')
-          .addTo(this._controller)
-      new ScrollMagic.Scene({ 
-            triggerElement: '.benefits-slider__copy2', 
-            triggerHook: 0.9, // show, when scrolled 10% into view
-            duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-            offset: 50 // move trigger to center of element
-          })
-          .setClassToggle('.benefits-slider__copy2', 'active')
-          .addTo(this._controller)
-      new ScrollMagic.Scene({ 
-            triggerElement: '.benefits-slider__copy3', 
-            triggerHook: 0.9, // show, when scrolled 10% into view
-            duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-            offset: 50 // move trigger to center of element
-          })
-          .setClassToggle('.benefits-slider__copy3', 'active')
-          .addTo(this._controller)
-      new ScrollMagic.Scene({ 
-            triggerElement: '.benefits-slider__copy4',
-            triggerHook: 0.9, // show, when scrolled 10% into view
-            duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-            offset: 50 // move trigger to center of element 
-          })
-          .setClassToggle('.benefits-slider__copy4', 'active')
-          .addTo(this._controller)
+                      .on('progress', (e) => {
+                        if(e.progress > 0 && e.progress <= .25) {
+                          // copy1 & icon1 active
+                          this.removeActives()
+                          $('.benefits-slider__copy1').addClass('active')
+                          $('.benefits-slider__icon1').addClass('active')
+                        } else if(e.progress > .25 && e.progress <= .5) {
+                          // copy2 & icon2 active
+                          this.removeActives()
+                          $('.benefits-slider__copy2').addClass('active')
+                          $('.benefits-slider__icon2').addClass('active')
+                        } else if(e.progress > .5 && e.progress <= .75) {
+                          // copy3 & icon3 active
+                          this.removeActives()
+                          $('.benefits-slider__copy3').addClass('active')
+                          $('.benefits-slider__icon3').addClass('active')
+                        } else if(e.progress > .75 && e.progress <= 1) {
+                          // copy 4 & icon4 active
+                          this.removeActives()
+                          $('.benefits-slider__copy4').addClass('active')
+                          $('.benefits-slider__icon4').addClass('active')
+                        }
+                      })
+    })
+  }
 
-
-      new ScrollMagic.Scene({ 
-            triggerElement: '.benefits-slider__copy1', 
-            triggerHook: 0.9, // show, when scrolled 10% into view
-            duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-            offset: 50, // move trigger to center of element
-          })
-          .setClassToggle('.benefits-slider__icon1', 'active')
-          .addTo(this._controller)
-      new ScrollMagic.Scene({ 
-            triggerElement: '.benefits-slider__copy2', 
-            triggerHook: 0.9, // show, when scrolled 10% into view
-            duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-            offset: 50 // move trigger to center of element
-          })
-          .setClassToggle('.benefits-slider__icon2', 'active')
-          .addTo(this._controller)
-      new ScrollMagic.Scene({ 
-            triggerElement: '.benefits-slider__copy3', 
-            triggerHook: 0.9, // show, when scrolled 10% into view
-            duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-            offset: 50 // move trigger to center of element
-          })
-          .setClassToggle('.benefits-slider__icon3', 'active')
-          .addTo(this._controller)
-      new ScrollMagic.Scene({ 
-            triggerElement: '.benefits-slider__copy4',
-            triggerHook: 0.9, // show, when scrolled 10% into view
-            duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-            offset: 50 // move trigger to center of element 
-          })
-          .setClassToggle('.benefits-slider__icon4', 'active')
-          .addTo(this._controller)
+  removeActives() {
+    $('.active').each((index, element) => {
+      $(element).removeClass('active')
     })
   }
 }
