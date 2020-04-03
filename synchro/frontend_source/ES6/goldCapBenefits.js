a3b7d46f332e43276e24c2d299984ddc69bf28b2
Syed AtiF Husain <atif@afstudios.me>
2020-04-04 03:30:06 +0800

    Edited cap benefits animation

--- synchro/frontend_source/ES6/goldCapBenefits.js
+++ synchro/frontend_source/ES6/goldCapBenefits.js
@@ -13,26 +13,37 @@ export default class GoldCapBenefits {
                         if(e.progress > 0 && e.progress <= .2) {
                           // copy0 & no-icon active
                           this.removeActives()
+                          this.removeVisited()
                           $('.benefits-slider__copy0').addClass('active')
                         } else if(e.progress > .2 && e.progress <= .4) {
                           // copy1 & icon1 active
                           this.removeActives()
+                          this.removeVisited()
                           $('.benefits-slider__copy1').addClass('active')
                           $('.benefits-slider__icon1').addClass('active')
                         } else if(e.progress > .4 && e.progress <= .6) {
                           // copy2 & icon2 active
                           this.removeActives()
+                          this.removeVisited()
                           $('.benefits-slider__copy2').addClass('active')
+                          $('.benefits-slider__icon1').addClass('visited')
                           $('.benefits-slider__icon2').addClass('active')
                         } else if(e.progress > .6 && e.progress <= .8) {
                           // copy3 & icon3 active
                           this.removeActives()
+                          this.removeVisited()
                           $('.benefits-slider__copy3').addClass('active')
+                          $('.benefits-slider__icon1').addClass('visited')
+                          $('.benefits-slider__icon2').addClass('visited')
                           $('.benefits-slider__icon3').addClass('active')
                         } else if(e.progress > .8 && e.progress <= 1) {
                           // copy 4 & icon4 active
                           this.removeActives()
+                          this.removeVisited()
                           $('.benefits-slider__copy4').addClass('active')
+                          $('.benefits-slider__icon1').addClass('visited')
+                          $('.benefits-slider__icon2').addClass('visited')
+                          $('.benefits-slider__icon3').addClass('visited')
                           $('.benefits-slider__icon4').addClass('active')
                         }
                       })
@@ -42,6 +53,11 @@ export default class GoldCapBenefits {
   removeActives() {
     $('.active').each((index, element) => {
       $(element).removeClass('active')
+    })
+  }
+  removeVisited() {
+    $('.visited').each((index, element) => {
+      $(element).removeClass('visited')
     })
   }
 }
